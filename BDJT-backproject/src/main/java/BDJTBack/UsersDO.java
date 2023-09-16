ca94a996bb4ebc48c98753eacbd41c1413cee339
Youngjin627 <inf.Youngjin@gmail.com>
2023-09-17 07:59:53 +0900

    UsersDO/BoardDO

--- BDJT-backproject/src/main/java/BDJTBack/UsersDO.java
+++ BDJT-backproject/src/main/java/BDJTBack/UsersDO.java
@@ -1,5 +1,42 @@
 package BDJTBack;
 
 public class UsersDO {
-
+	private String id;
+	private String name;
+	private String password;
+	private String birthdate;
+	private String github;
+	
+	public TestDO () {
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getGithub() {
+		return github;
+	}
+	public void setGithub(String github) {
+		this.github = github;
+	}
 }
