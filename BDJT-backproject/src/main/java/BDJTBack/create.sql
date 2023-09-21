52fb489e7323036b2c70c88a215beeef5c1f7d7b
9a_orn <Lum.Gayoung@gmail.com>
2023-09-21 17:53:05 +0900

    샘플데이터

--- BDJT-backproject/src/main/java/BDJTBack/create.sql
+++ BDJT-backproject/src/main/java/BDJTBack/create.sql
@@ -1,7 +1,7 @@
 -- 회원 테이블 생성
 CREATE TABLE users (
     id VARCHAR2(15) PRIMARY KEY,
-    name VARCHAR2(5) NOT NULL,
+    name VARCHAR2(10) NOT NULL,
     password VARCHAR2(15) NOT NULL,
     birthdate DATE,
     github VARCHAR2(30) 
@@ -20,25 +20,85 @@ CREATE TABLE board (
 
 -- 회원 테이블 샘플
 INSERT INTO users (id, name, password, birthdate, github)
-VALUES ('user1', 'John', 'password1', TO_DATE('1990-05-15', 'YYYY-MM-DD'), 'github@gmail.com');
+VALUES ('user1', 'John', 'password1', TO_DATE('1990-05-15', 'YYYY-MM-DD'), 'https://github.com/user1');
 
 INSERT INTO users (id, name, password, birthdate, github)
 VALUES ('user2', 'Jane', 'p', TO_DATE('1985-09-22', 'YYYY-MM-DD'), 'https://github.com/user2');
 
 INSERT INTO users (id, name, password, birthdate, github)
-VALUES ('user3', 'Bob', 'password3', TO_DATE('1995-03-10', 'YYYY-MM-DD'), 'https://github.com/user3');
+VALUES ('user3', 'billie', 'password3', TO_DATE('1991-01-10', 'YYYY-MM-DD'), 'https://github.com/user3');
 
+INSERT INTO users (id, name, password, birthdate, github)
+VALUES ('user4', 'mycle', 'password3', TO_DATE('1993-03-10', 'YYYY-MM-DD'), 'https://github.com/user4');
+
+INSERT INTO users (id, name, password, birthdate, github)
+VALUES ('user5', 'Michael', 'password3', TO_DATE('1985-03-10', 'YYYY-MM-DD'), 'https://github.com/user5');
+
+INSERT INTO users (id, name, password, birthdate, github)
+VALUES ('user6', 'chris', 'password3', TO_DATE('2001-03-10', 'YYYY-MM-DD'), 'https://github.com/user6');
 commit;
 
 -- 보드 테이블 샘플
-INSERT INTO board (photo, title, url, skill, creationdate, orderRcmnd, id)
-VALUES ('photo1.jpg', 'Sample Post 1', 'https://example.com/post1', 'Java', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 5, 'user1');
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test1.jpg', 'Sample Post 1', 'https://www.naver.com/', 'html', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
     
-INSERT INTO board (photo, title, url, skill, creationdate, orderRcmnd, id)
-VALUES ('photo2.jpg', 'Sample Post 2', 'https://example.com/post2', 'Python', TO_DATE('2023-09-14', 'YYYY-MM-DD'), 3, 'user2');
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test2.jpg', 'Sample Post 2', 'https://www.naver.com/', 'css', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
     
-INSERT INTO board (photo, title, url, skill, creationdate, orderRcmnd, id)
-VALUES ('photo3.jpg', 'Sample Post 3', 'https://example.com/post3', 'JavaScript', TO_DATE('2023-09-13', 'YYYY-MM-DD'), 7, 'user3');
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test3.jpg', 'Sample Post 3', 'https://www.naver.com/', 'css', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user2');
+    
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test4.jpg', 'Sample Post 4', 'https://www.naver.com/', 'html', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user4');
+    
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test5.jpg', 'Sample Post 5', 'https://www.naver.com/', 'react', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user4');
+    
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test6.jpg', 'Sample Post 6', 'https://www.naver.com/', 'html', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user6');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test7.jpg', 'Sample Post 7', 'https://www.naver.com/', 'react', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user5');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test8.jpg', 'Sample Post 8', 'https://www.naver.com/', 'react', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test9.jpg', 'Sample Post 9', 'https://www.naver.com/', 'html', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user2');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test10.jpg', 'Sample Post 10', 'https://www.naver.com/', 'html', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+
+VALUES ('/BDJT-backproject/upload/test11.jpg', 'Sample Post 11', 'https://www.naver.com/', 'css', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user6');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test12.jpg', 'Sample Post 12', 'https://www.naver.com/', 'css', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user3');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test13.jpg', 'Sample Post 13', 'https://www.naver.com/', 'html', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test14.jpg', 'Sample Post 14', 'https://www.naver.com/', 'react', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user5');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test15.jpg', 'Sample Post 15', 'https://www.naver.com/', 'react', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user4');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test16.jpg', 'Sample Post 16', 'https://www.naver.com/', 'html', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user3');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test17.jpg', 'Sample Post 17', 'https://www.naver.com/', 'react', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test18.jpg', 'Sample Post 18', 'https://www.naver.com/', 'css', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test19.jpg', 'Sample Post 19', 'https://www.naver.com/', 'css', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
+
+INSERT INTO board (photo, title, url, skill, creationdate, id)
+VALUES ('/BDJT-backproject/upload/test20.jpg', 'Sample Post 20', 'https://www.naver.com/', 'react', TO_DATE('2023-09-15', 'YYYY-MM-DD'), 'user1');
+
 
 commit;
 
